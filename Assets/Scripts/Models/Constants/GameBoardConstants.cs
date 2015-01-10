e5523309dbad0de0aa1daa43c3e5db6a58be0834
Denny Scott <dennyscott301@gmail.com>
2015-01-10 16:32:08 -0600

    Added Base GameBoardModel

---
+++ Assets/Scripts/Models/Constants/GameBoardConstants.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameBoardConstants {
+
+    public readonly static int ROWS = 10;
+    public readonly static int COLUMNS = 5;
+    
+}
--- Assets/Scripts/Models/GameBoard/GameBoardModel.cs
+++ Assets/Scripts/Models/GameBoard/GameBoardModel.cs
@@ -2,13 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameBoardModel : MonoBehaviour {
+public class GameBoardModel {
 
-	private List<List<ElementModel> y;
-	private List<ElementModel> x;
+	private List<List<GamePieceModel>> rows;
 
 	public GameBoardModel() {
+        rows = new List<List<GamePieceModel>>();
+        CreateGameBoard();
+    }
 
-	}
+    private void CreateGameBoard() {
+        for (int i = 0; i < GameBoardConstants.ROWS; i++) {
+            rows.Add(CreateRow());
+        }
+    }
+
+    private List<GamePieceModel> CreateRow(){
+        List<GamePieceModel> row = new List<GamePieceModel>();
+        
+        for (int i = 0; i < GameBoardConstants.COLUMNS; i++) {
+            row.Add(new GamePieceModel(new GameObject()));
+        }
+
+        return row;
+    }
+
+    public void AddGamePiece(GamePieceModel gp, int x, int y){
+        rows[y][x] = gp;
+    }
 
 }
