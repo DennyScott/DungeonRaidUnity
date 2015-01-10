bc71c3697b1e609ca699e635e74f1fc3ca6b4f67
Denny Scott <dennyscott301@gmail.com>
2015-01-10 17:13:17 -0600

    Added Testing for GameBoardMOdel

--- Assets/Editor/Tests/Unit/Models/GameBoard/GameBoardTest.cs
+++ Assets/Editor/Tests/Unit/Models/GameBoard/GameBoardTest.cs
@@ -3,7 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 
+[TestFixture]
+[Category("Game Board Testing")]
 public class GameBoardTest {
 
+    GameBoardModel gameboard;
+    GamePieceModel gamePiece;
 
+    [SetUp]
+    public void Init() {
+        gameboard = new GameBoardModel();
+        gamePiece = new GamePieceModel(new GameObject());
+    }
+
+    [Test]
+    public void CorrectAmountOfRowsCreated() {
+        Assert.AreEqual(GameBoardConstants.ROWS, gameboard.GetRows().Count);
+    }
+
+    [Test]
+    public void CorrectAmountOfColumnsCreated() {
+        Assert.AreEqual(GameBoardConstants.COLUMNS, gameboard.GetRow(0).Count);
+    }
+
+    [Test]
+    public void AddingGamePieceWillPlaceIntoGameBoard() {
+        gameboard.AddGamePiece(gamePiece, 0, 0);
+        Assert.AreEqual(gamePiece, gameboard.GetGamePiece(0, 0));
+    }
 }
--- Assets/Scripts/Models/GameBoard/GameBoardModel.cs
+++ Assets/Scripts/Models/GameBoard/GameBoardModel.cs
@@ -27,8 +27,20 @@ public class GameBoardModel {
         return row;
     }
 
-    public void AddGamePiece(GamePieceModel gp, int x, int y){
+    public List<List<GamePieceModel>> GetRows() {
+        return rows;
+    }
+
+    public List<GamePieceModel> GetRow(int row){
+        return GetRows()[row];
+    }
+
+    public void AddGamePiece(GamePieceModel gp, int y, int x){
         rows[y][x] = gp;
+    }
+
+    public GamePieceModel GetGamePiece(int y, int x){
+        return rows[y][x];
     }
 
 }
