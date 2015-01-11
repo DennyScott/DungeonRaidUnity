91eca6418900d268e2a9e46c5430a8f148dfc2c5
Travis Scott <travisscott301@gmail.com>
2015-01-10 19:48:44 -0600

    Created Pieces and Controller

---
+++ Assets/Scripts/Controllers/GameBoardController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameBoardController : IGameBoardController {
+
+	public bool CheckForMatch(GamePieceModel from, GamePieceModel to) {
+		return false;
+	}
+	
+	public List<GamePieceModel> FillGameBoard() {
+		return new List<GamePieceModel>();
+	}
+	
+	public void ChangeTypeToType(GamePieceModel fromType, GamePieceModel toType) {
+		
+	}
+	
+	public void RemoveType(GamePieceModel type) {
+		
+	}
+
+}
--- Assets/Scripts/Managers/GameBoardManager.cs
+++ Assets/Scripts/Managers/GameBoardManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameBoardManager : MonoBehaviour {
-
-
+public class GameBoardManager : MonoBehaviour{
+	GameBoardController gameBoardController = new GameBoardController();
 }
