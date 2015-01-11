8a405a308378d2492181773d18a43eee4fb920e6
Travis Scott <travisscott301@gmail.com>
2015-01-10 20:48:44 -0600

    Updated GameBoardController tests and interface

--- Assets/Editor/Tests/Unit/Controllers/GameBoard/GameBoardControllerTest.cs
+++ Assets/Editor/Tests/Unit/Controllers/GameBoard/GameBoardControllerTest.cs
@@ -21,13 +21,37 @@ public class GameBoardControllerTest {
 	[SetUp]
 	public void Init() {
 		gameBoardController = new GameBoardController();
-		gameboard = new GameBoardModel();
 		firePieceOne = new FireModel();
 		firePieceTwo = new FireModel();
+		waterPieceOne = new WaterModel();
+		waterPieceTwo = new WaterModel();
+		earthPieceOne = new EarthModel();
+		earthPieceTwo = new EarthModel();
+		airPieceOne = new AirModel();
+		airPieceTwo = new AirModel();
 	}
-//	
-//	[Test]
-//	public void CheckForMatchOnMatchIsTrue() {
-//		Assert.IsTrue(gameBoardController.CheckForMatch(gamePieceOne, gamePieceTwo));
-//	}
+	
+	[Test]
+	public void CheckForMatchOnMatchIsTrue() {
+		Assert.IsTrue(gameBoardController.CheckForMatch(firePieceOne, firePieceTwo));
+		Assert.IsTrue(gameBoardController.CheckForMatch(waterPieceOne, waterPieceTwo));
+		Assert.IsTrue(gameBoardController.CheckForMatch(earthPieceOne, earthPieceTwo));
+		Assert.IsTrue(gameBoardController.CheckForMatch(airPieceOne, airPieceTwo));
+	}
+
+	[Test]
+	public void CheckForMatchOnNonMatchIsNotTrue() {
+		Assert.IsTrue(gameBoardController.CheckForMatch(firePieceOne, waterPieceTwo));
+		Assert.IsTrue(gameBoardController.CheckForMatch(waterPieceOne,earthPieceTwo ));
+		Assert.IsTrue(gameBoardController.CheckForMatch(earthPieceOne, airPieceTwo));
+		Assert.IsTrue(gameBoardController.CheckForMatch(airPieceOne, firePieceTwo));
+	}
+
+	[Test]
+	public void CheckFillEmptyFillGameBoardModel() {
+		gameBoardController.Clear();
+		gameBoardController.FillGameBoard();
+		Assert.IsFalse(gameBoardController.isGameboardEmpty());
+		Assert.IsTrue(gameBoardController.isGameboardFull())
+	}
 }
--- Assets/Scripts/Controllers/GameBoardController.cs
+++ Assets/Scripts/Controllers/GameBoardController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameBoardController : IGameBoardController {
+	GameBoardModel gameBoardModel;
+
+	public GameBoardController() {
+		gameBoardModel = new GameBoardModel();
+	}
 
 	public bool CheckForMatch(GamePieceModel from, GamePieceModel to) {
 		return false;
 	}
 	
-	public List<GamePieceModel> FillGameBoard() {
-		return new List<GamePieceModel>();
+	public void FillGameBoard() {
+
 	}
 	
 	public void ChangeTypeToType(GamePieceModel fromType, GamePieceModel toType) {
@@ -18,6 +23,18 @@ public class GameBoardController : IGameBoardController {
 	
 	public void RemoveType(GamePieceModel type) {
 		
+	}
+
+	public void ClearGameBoard() {
+
+	}
+
+	public bool IsGameBoardEmpty() {
+		return false;
+	}
+
+	public bool IsGameBoardFull() {
+		return false;
 	}
 
 }
