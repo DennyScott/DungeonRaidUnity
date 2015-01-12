2cce0474b8c2ee1d739c95bdb89395f60b94c3e3
Travis Scott <travisscott301@gmail.com>
2015-01-11 23:53:53 -0600

    Updated Controller and Manager

--- Assets/Scripts/Managers/GameBoardManager.cs
+++ Assets/Scripts/Managers/GameBoardManager.cs
@@ -3,15 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameBoardManager : MonoBehaviour{
-	GameBoardController gameBoardController;
+	private GameBoardModel _gameBoardModel;
+	private List<GameObject> gamePieces;
+
 	public int rows = 10;
 	public int columns = 5;
+	
+	GameBoardModel gameBoardModel {
+		get {
+			return _gameBoardModel;
+		}
+	}
 
 	void Awake() {
-		gameBoardController = new GameBoardController(rows, columns);
+		_gameBoardModel = new GameBoardModel(rows, columns);
+		CreateBoardViewPieces();
 	}
 
 	void Start() {
-//		gameBoardController.CheckForMatch();
+
 	}
+
+	void CreateBoardViewPieces() {
+		List<List<GamePieceModel>> board = gameBoardModel.GetBoard();
+		foreach (List<GamePieceModel> row in board) {
+			foreach (GamePieceModel piece in row) {
+				Debug.Log (piece.ToString());
+			}
+		}
+	}
+
+
 }
