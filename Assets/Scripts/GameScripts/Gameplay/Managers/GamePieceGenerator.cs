2170ae5bd48fd476d8e12565be742eefd9f81c8a
Travis Scott <travisscott301@gmail.com>
2015-03-13 23:02:20 -0500

    Updated GamePieceGenerator to better standards

--- Assets/Scripts/GameScripts/Gameplay/Managers/GamePieceGenerator.cs
+++ Assets/Scripts/GameScripts/Gameplay/Managers/GamePieceGenerator.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
 public class GamePieceGenerator : MonoBehaviour {
 
 	#region Public Varables
-	public  GameObject airElement;
-	public  GameObject fireElement;
-	public  GameObject earthElement;
-	public  GameObject waterElement;
-	public GameObject gamePiece;
-	public float blockSpacing;
-	public float offset = 0.5f;
-	public List<GameObject> positions;
+	public GameObject _airElement;
+	public GameObject _fireElement;
+	public GameObject _earthElement;
+	public GameObject _waterElement;
+	public GameObject _gamePiece;
+	public float _blockSpacing;
+	public float _offset = 0.5f;
+	public List<GameObject> _positions;
 
 	#endregion
 
@@ -53,8 +52,8 @@ public class GamePieceGenerator : MonoBehaviour {
 	/// <param name="x">The x coordinate.</param>
 	/// <param name="y">The y coordinate.</param>
 	private GameObject CreateElement(GameObject element, float x, float y) {
-		GameObject newPiece = Instantiate(gamePiece) as GameObject;
-		GameObject newGraphic = Instantiate(element) as GameObject;
+		var newPiece = Instantiate(_gamePiece);
+		var newGraphic = Instantiate(element);
 		newGraphic.transform.parent = newPiece.transform;
 		newGraphic.transform.position = new Vector3(0, 0, 0);
 		newPiece.transform.position = new Vector3(x, y, 0.0f);
@@ -69,20 +68,28 @@ public class GamePieceGenerator : MonoBehaviour {
 	/// <param name="y">The y coordinate.</param>
 	public void CreatePiece(int x, int y) {
 		CheckManagers();
-		float startX = (float) x - 4 + offset;
-		float startY = 5.0f;
+		float startX = (float) x - 4 + _offset;
+		const float startY = 5.0f;
 		float endX = startX;
-		float endY = (float)y - 4 + offset;
+		float endY = (float)y - 4 + _offset;
 		int r = Random.Range(0, 4);
-		GameObject newPiece = airElement;
-		if(r == 0) {
-			newPiece = airElement;
-		} else if (r == 1) {
-			newPiece = waterElement;
-		} else if (r == 2) {
-			newPiece = fireElement;
-		} else if (r == 3) {
-			newPiece = earthElement;
+		GameObject newPiece;
+		switch (r) {
+			case 0:
+				newPiece = _airElement;
+				break;
+			case 1:
+				newPiece = _waterElement;
+				break;
+			case 2:
+				newPiece = _fireElement;
+				break;
+			case 3:
+				newPiece = _earthElement;
+				break;
+			default:
+			  	newPiece = _airElement;
+				break;
 		}
 
 		newPiece = CreateElement(newPiece, startX, startY);
