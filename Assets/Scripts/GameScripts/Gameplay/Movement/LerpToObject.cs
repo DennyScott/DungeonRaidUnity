98fafa04da72ce0505b0483a32ffa61b0809444d
Denny Scott <dennyscott301@gmail.com>
2015-03-13 23:14:29 -0500

    update

---
+++ Assets/Scripts/GameScripts/Gameplay/Movement/LerpToObject.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpToObject : MonoBehaviour {
+
+	#region Private Variables
+	private Vector3 productFacing;
+	private Vector3 handleDestination;
+	private GameObject target;
+	private float lerpStartTime;
+	#endregion
+
+	#region Public Variables
+	public float distanceFromProduct = 2.0f;
+	public float lerpSpeed = 1f;
+	#endregion
+
+
+	#region Delegates and Events
+	public delegate void LerpAction();
+	public event LerpAction LerpCompleted;
+
+	public delegate bool IsAcceptable(GameObject go);
+	public event IsAcceptable CanMove;
+	#endregion
+
+	#region Enumerators
+
+	/// <summary>
+	/// Lerp the current object to the target game object. Once the perp has been completed
+	/// we will trigger the lerpComleted event.
+	/// </summary>
+	IEnumerator LerpToDefault() {
+		while (Time.time < lerpStartTime + lerpSpeed) {
+			transform.position = Vector3.Lerp(transform.position, handleDestination, GetTimeForLerp());
+			yield return null;
+		}
+		transform.position = handleDestination;
+		TriggerLerpCompleted();
+	}
+
+	#endregion
+
+	#region Private Methods
+	/// <summary>
+	/// Get the Time needed for the lerp. It will take the current time - the original 
+	/// start time, and divide it be the speed.
+	/// </summary>
+	private float GetTimeForLerp() {
+		return (Time.time - lerpStartTime)/lerpSpeed;
+	}
+
+	/// <summary>
+	/// Sets variables necessary for handle to begin lerping. 
+	/// Store the necessary variables and then begin the lerp coroutine.
+	/// </summary>
+	private void BeginHandleLerp() {
+		lerpStartTime = Time.time;
+		productFacing = target.transform.forward;
+		FindHandleDestination();
+		StartCoroutine("LerpToDefault");
+	}
+
+	/// <summary>
+	/// Ends the lerp coroutine
+	/// </summary>
+	private void StopHandleLerp() {
+		StopCoroutine("LerpToDefault");
+	}
+
+	/// <summary>
+	/// Finds where the handle should be reset to
+	/// </summary>
+	private void FindHandleDestination() {
+		handleDestination = Camera.main.WorldToScreenPoint(target.transform.position + distanceFromProduct*productFacing);
+	}
+	#endregion
+
+	#region Triggers
+	/// <summary>
+	/// The Lerp has completed, so we call the Lerp Completed Delegate, to call any methods
+	/// waiting to be called after animation.
+	/// </summary>
+	private void TriggerLerpCompleted() {
+		if (LerpCompleted != null) {
+			LerpCompleted();
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+	/// <summary>
+	///  Take the Game Object this component is attached to, and lerp it to the GameObject
+	///  that is passed. You must also dictate the speed and the distance from the product
+	///  the lerp needs to stop at.
+	/// </summary>
+	/// <param name="target">
+	/// Game Object that "this" game object will lerp to.
+	/// </param> 
+	/// <param name="lerpSpeed">
+	/// Speed to lerp the GameObject at, in seconds.
+	/// </param>
+	/// <param name="distanceFromProduct">
+	/// The distance from the target to stop the lerp at.
+	/// </param>
+	public void LerpTo(GameObject target, float lerpSpeed, float distanceFromProduct) {
+		//Set Instance Variables
+		this.lerpSpeed = lerpSpeed;
+		this.distanceFromProduct = distanceFromProduct;
+		this.target = target;
+
+		//Start Lerp
+		if(CanMove == null || CanMove(gameObject)){
+			BeginHandleLerp();
+		}
+	}
+
+	/// <summary>
+	/// Ceases the lerp action of an object immediatley, needed for certain interrupts
+	/// </summary>
+	public void StopLerpTo() {
+		StopHandleLerp();
+	}
+#endregion
+}
+
+
---
+++ Assets/Scripts/GameScripts/Gameplay/Movement/RotateToObject.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Rotate the current game object to a desired target game object. This will not inately turn
+/// a game object, but must be called externally through one of its public apis.
+/// </summary>
+public class RotateToObject : MonoBehaviour {
+
+#region States
+	public enum ObjectState {IDLE, IS_ROTATING};
+	public ObjectState currentState = ObjectState.IDLE;
+#endregion
+
+#region Private Variables
+	private GameObject target;
+	private Vector3 thisRotation;
+#endregion
+
+#region Delegates and Events
+	public delegate bool IsAcceptable(GameObject g);
+	public delegate void RotationEvent(GameObject g);
+
+	public event IsAcceptable CanMove;
+	public event IsAcceptable IsDropable;
+	public event RotationEvent OnRotationStart;
+	public event RotationEvent OnRotationFinish;
+
+#endregion
+
+#region Public Methods
+	/// <summary>
+	/// Rotate the gameObject that this component is attached to the target
+	/// gameObject that is passed.
+	/// </summary>
+	/// <param name="target">
+	/// GameObject to rotate to
+	/// </param>
+	public void SingleRotateTo(GameObject target) {
+		this.target = target;
+		if(CanMove == null || CanMove(gameObject)){
+			TriggerOnRotationStart();
+			RotateObject();
+			TriggerOnRotationFinish();
+		}
+	}
+
+	/// <summary>
+	/// Continously rotate the gameObjec tthat this component is attached to, to the 
+	/// target gameObject that is passed. This is done through a coroutine.
+	/// </summary>
+	/// <param name="target">
+	/// GameObject to Rotate to
+	/// </param>
+	public void ContinuousRotateTo(GameObject target) {
+		this.target = target;
+		if(CanMove == null || CanMove(gameObject)){
+			TriggerOnRotationStart();
+			StartCoroutine("RunRotateObject");
+		}
+	}
+
+	/// <summary>
+	/// Stop the rotation coroutine from running.
+	/// </summary>
+	public void StopRotate() {
+		StopCoroutine("RunRotateObject");
+		TriggerOnRotationFinish();
+	}
+#endregion
+
+#region Event Tiggers
+	/// <summary>
+	/// Trigger On Rotation Start. This will set the current state to IS_ROTATING and then
+	/// call any subs in the OnRotationStart event.
+	/// </summary>
+	void TriggerOnRotationStart() {
+		currentState = ObjectState.IS_ROTATING;
+		if(OnRotationStart != null){
+			OnRotationStart(gameObject);
+		}
+	}
+
+	/// <summary>
+	/// Trigger On Rotation Finish. This will set the current state to IDLE and then call
+	/// any subs in the OnRotationFinish event.
+	/// </summary> 
+	void TriggerOnRotationFinish() {
+		currentState = ObjectState.IDLE;
+		if(OnRotationFinish != null){
+			OnRotationFinish(gameObject);
+		}
+	}
+#endregion
+
+#region Private Methods
+	/// <summary>
+	/// Finds the angle necessary to let the button face the product
+	/// </summary>
+	private void FindAngle() {
+		thisRotation = transform.eulerAngles;
+		thisRotation.z = -target.transform.eulerAngles.y + 180;
+	}
+
+	/// <summary>
+	/// Rotate the current gameObject to the target game object.
+	/// </summary>
+	private void RotateObject() {
+		FindAngle();
+		transform.eulerAngles = thisRotation;
+	}
+#endregion
+
+#region Coroutines
+	/// <summary>
+	/// Continue to call the RotateObject method, turning the current game object to
+	/// the target game object.
+	/// </summary>
+	IEnumerator RunRotateObject() {
+		while(true) {
+			RotateObject();
+			yield return null;
+
+		}
+	}
+#endregion
+}
