b9f2130af23031d4041a9fdc52b8d8a460196aeb
Nicholas Reid <nick3reid@gmail.com>
2015-06-03 22:26:10 -0500

    Added mousedown

--- Assets/Scripts/GameScripts/Gameplay/New Items/Mouse.cs
+++ Assets/Scripts/GameScripts/Gameplay/New Items/Mouse.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mouse : MonoBehaviour {
+public class Mouse : Grunt {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Vector3 pos;
+    Vector3 scale = new Vector3(1,1,1);
+    
+
+    // Use this for initialization
+    void Start() {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMouseDown() {
+        if (clicked) {
+            Debug.Log("Clicked, " + scale);
+
+            pos += Vector3.forward;
+            scale += new Vector3(0.5f, 0.5f, 0.5f);
+
+            this.gameObject.transform.position = pos;
+            this.gameObject.transform.localScale = scale;
+        }
+
+
+    }
 }
