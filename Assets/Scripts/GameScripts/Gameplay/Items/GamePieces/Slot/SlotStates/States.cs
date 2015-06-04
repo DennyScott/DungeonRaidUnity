fa68fb832b9cd3ac51b04b48b0316522cfbbf00e
Denny Scott <DennyScott301@gmail.com>
2015-06-03 21:02:50 -0500

    Added new elements to framework

---
+++ Assets/Scripts/GameScripts/Gameplay/Items/GamePieces/Slot/SlotStates/States.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class Slot {
+
+	public class ConcreteState : IFSMState {
+		public virtual void OnEntry() {}
+		public virtual void OnExit() {}
+	}
+
+	public class NormalState : ConcreteState {
+		public override void OnEntry() {}
+		public override void OnExit() {}	
+	}
+
+	public class EmptyState : ConcreteState {
+		public override void OnEntry() {}
+		public override void OnExit() {}
+	}
+}
