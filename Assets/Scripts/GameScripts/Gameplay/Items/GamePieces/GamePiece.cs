50aa7a816c8f0260c548ff3c5e0284a8e90b3a54
Travis Scott <travisscott301@gmail.com>
2015-06-03 22:26:14 -0500

    Updated Movement algorithms

--- Assets/Scripts/GameScripts/Gameplay/Items/GamePieces/GamePiece.cs
+++ Assets/Scripts/GameScripts/Gameplay/Items/GamePieces/GamePiece.cs
@@ -1,22 +1,12 @@
 ﻿using UnityEngine;
 
 public class GamePiece : Grunt {
-	
-	#region Private Variables
-	private bool isLerping;
-	private Vector3 startPosition;
-	private Vector3 endPosition;
-	private float startTime;
-	private float journeyLength;
-	private int _row;
-	private int _column;
-	#endregion
 
-	#region Public Variables
-	public float _speed = 1.0F;
-	public float _smooth = 5.0F;
+	public int Row { get; set; }
 
-	#endregion
+	public int Column { get; set; }
+
+
 
 	#region Delegates
 	public delegate void GamePieceEvent(GameObject g);
@@ -28,62 +18,10 @@ public class GamePiece : Grunt {
 	public event GamePieceEvent OnClickUp;
 	public event GamePieceEvent OnMouseEnterPiece;
 	public event GamePieceEvent OnMouseExitPiece;
-	public event GamePieceEvent OnStartLerp;
-	public event GamePieceEvent OnStopLerp;
 	public event GamePieceEvent OnRemovePiece;
 	#endregion
-	
-	#region Standard Methods
-	void FixedUpdate () {
-		if (isLerping) {
-			float distCovered = (Time.time - startTime) * _speed;
-			float fracJourney = distCovered / journeyLength;
-			transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
 
-			if(fracJourney>=1.0f) {
-				StopLerp();
-			}
-		}
-	}
-	#endregion
-
-	#region Movement Controls
-
-	/// <summary>
-	/// Sets the position of the piece in terms of column and row
-	/// </summary>
-	/// <param name="row">The game pieces row.</param>
-	/// <param name="column">The game pieces column.</param>
-	public void SetPosition(int row, int column) {
-		_row = row;
-		_column = column;
-	}
-
-	/// <summary>
-	/// Stops the lerping of this object, as well as emits the OnStopLerp event
-	/// </summary>
-	public void StopLerp() {
-		isLerping = false;
-		if(OnStopLerp != null) {
-			OnStopLerp(gameObject);
-		}
-	}
-
-	/// <summary>
-	/// Starts the lerp for this object, as well as emits the OnStartLerpEvent
-	/// </summary>
-	/// <param name="endPosition">End position.</param>
-	public void StartLerp(Vector3 endPosition) {
-		isLerping = true;
-		startPosition = transform.position;
-		this.endPosition = endPosition;
-		startTime = Time.time;
-		journeyLength = Vector3.Distance(startPosition, this.endPosition);
-		if(OnStartLerp != null) {
-			OnStartLerp(gameObject);
-		}
-	}
-
+	#region Public Methods
 	/// <summary>
 	/// 	Used to remove a piece from the gameboard
 	/// </summary>
@@ -91,57 +29,12 @@ public class GamePiece : Grunt {
 		TriggerOnRemovePiece();
 		Destroy(gameObject);
 	}
+
 	#endregion
 
-	#region Event Handling
-
-	/// <summary>
-	/// Raises the mouse down event, and emits the OnClickDown event.
-	/// </summary>
-	void OnMouseDown() {
-		if(OnClickDown != null) {
-			OnClickDown(gameObject);
-		}
-	}
-
-	/// <summary>
-	/// Raises the mouse up event and emits the OnClickUp event.
-	/// </summary>
-	void OnMouseUp() {
-		if(OnClickUp != null) {
-			OnClickUp(gameObject);
-		}
-	}
-
-	/// <summary>
-	/// Raises the mouse enter event, and emits the OnMouseEnterPiece event.
-	/// </summary>
-	void OnMouseEnter() {
-		if(OnMouseEnterPiece != null) {
-			OnMouseEnterPiece(gameObject);
-		}
-	}
-
-	/// <summary>
-	/// Raises the mouse exit event, and emits the OnMouseExitPiece event.
-	/// </summary>
-	void OnMouseExit() {
-		if(OnMouseExitPiece != null) {
-			OnMouseExitPiece(gameObject);
-		}
-	}
-
-	/// <summary>
-	/// 	Removes the gameObject from the board, and calls all listening handlers
-	/// </summary>
 	void TriggerOnRemovePiece() {
-		if(OnRemovePiece != null) {
+		if (OnRemovePiece != null) {
 			OnRemovePiece(gameObject);
 		}
 	}
-	#endregion
-
-	public int Row {get; set;}
-
-	public int Column{get; set;}
 }
--- Assets/Scripts/GameScripts/Gameplay/Managers/GamePieceManager.cs
+++ Assets/Scripts/GameScripts/Gameplay/Managers/GamePieceManager.cs
@@ -9,22 +9,16 @@ public class GamePieceManager : Manager {
 	
 	#endregion
 
-	#region Delegates
-	public delegate void GamePieceEvent(GameObject g);
-	public delegate void MovementEvent();
-
-	#endregion
-
 	#region Events
-	public event GamePieceEvent OnClickDown;
-	public event GamePieceEvent OnClickUp;
-	public event GamePieceEvent OnMouseEnterPiece;
-	public event GamePieceEvent OnMouseExitPiece;
-	public event GamePieceEvent OnStartLerp;
-	public event GamePieceEvent OnStopLerp;
-	public event GamePieceEvent OnRemovePiece;
-	public event MovementEvent OnPiecesMoving;
-	public event MovementEvent OnPiecesStopped;
+	public System.Action<GameObject> OnClickDown;
+	public System.Action<GameObject> OnClickUp;
+	public System.Action<GameObject> OnMouseEnterPiece;
+	public System.Action<GameObject> OnMouseExitPiece;
+	public System.Action<GameObject> OnGamePieceStartMove;
+	public System.Action<GameObject> OnGamePieceStopMove;
+	public System.Action<GameObject> OnRemovePiece;
+	public System.Action OnPiecesMoving;
+	public System.Action OnPiecesStopped;
 
 	#endregion
 
@@ -35,21 +29,23 @@ public class GamePieceManager : Manager {
 	/// <param name="piece">Game Piece to Register</param>
 	public void RegisterPiece(GameObject piece) {
 		var gamePiece = piece.GetComponent<GamePiece>();
+		var gpMovement = piece.GetComponent<GamePieceMovement>();
 		gamePiece.OnClickDown += HandleOnClickDown;
 		gamePiece.OnClickUp += HandleOnClickUp;
 		gamePiece.OnMouseEnterPiece += HandleOnMouseEnterPiece;
-		gamePiece.OnStartLerp += HandleOnStartLerp;
-		gamePiece.OnStopLerp += HandleOnStopLerp;
+		gpMovement.OnGamePieceStartMove += HandleOnGamePieceStartMove;
+		gpMovement.OnGamePieceStopMove += HandleOnGamePieceStopMove;
 		gamePiece.OnRemovePiece += HandleOnRemovePiece;
 	}
 
 	public void UnRegisterPiece(GameObject piece) {
 		var gamePiece = piece.GetComponent<GamePiece>();
+		var gpMovement = piece.GetComponent<GamePieceMovement>();
 		gamePiece.OnClickDown -= HandleOnClickDown;
 		gamePiece.OnClickUp -= HandleOnClickUp;
 		gamePiece.OnMouseEnterPiece -= HandleOnMouseEnterPiece;
-		gamePiece.OnStartLerp -= HandleOnStartLerp;
-		gamePiece.OnStopLerp -= HandleOnStopLerp;
+		gpMovement.OnGamePieceStartMove -= HandleOnGamePieceStartMove;
+		gpMovement.OnGamePieceStopMove -= HandleOnGamePieceStopMove;
 		gamePiece.OnRemovePiece -= HandleOnRemovePiece;
 	}
 
@@ -66,7 +62,7 @@ public class GamePieceManager : Manager {
 	/// Handles the on stop lerp event.
 	/// </summary>
 	/// <param name="g">The gameObject that has just stopped lerping.</param>
-	void HandleOnStopLerp (GameObject g) {
+	void HandleOnGamePieceStopMove(GameObject g) {
 		movingObjects.Remove(g);
 		if(movingObjects.Count == 0 && OnPiecesStopped != null) {
 			OnPiecesStopped();
@@ -77,10 +73,10 @@ public class GamePieceManager : Manager {
 	/// Handles the on start lerp event.
 	/// </summary>
 	/// <param name="g">The gameObject that has just started lerping.</param>
-	void HandleOnStartLerp (GameObject g) {
+	void HandleOnGamePieceStartMove(GameObject g) {
 		movingObjects.Add(g);
-		if(OnStartLerp != null) {
-			OnStartLerp(g);
+		if(OnGamePieceStartMove != null) {
+			OnGamePieceStartMove(g);
 		}
 
 		if(OnPiecesMoving != null) {
@@ -131,8 +127,10 @@ public class GamePieceManager : Manager {
 	/// <param name="positionY">The y position to end at</param>
 	public static void MovePiece(GameObject piece, int x, int y, float positionX, float positionY) {
 		var gamePiece = piece.GetComponent<GamePiece>();
-		gamePiece.SetPosition(x, y);
-		gamePiece.StartLerp(new Vector3 (positionX, positionY, 0.0f));
+		var gpMovement = piece.GetComponent<GamePieceMovement>();
+		gamePiece.Row = x;
+		gamePiece.Column = y;
+		gpMovement.StartLerp(new Vector3(positionX, positionY, 0.0f));
 	}
 
 	#endregion
